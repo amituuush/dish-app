5c8ba0d332887d70493fae4eb67f80ed91815415
Amit Ranan <ranan.amit@gmail.com>
2017-09-17 18:06:58 -0700

    completed menu search and created FoodItem component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import { config } from './config.js';
-import logo from './logo.svg';
+
+import { FoodItem } from './components/FoodItem.js';
 import './App.css';
 
 var proxyUrl = 'https://cors-anywhere.herokuapp.com/';
 const googleUrl = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=34.1820822,-118.7839273&radius=3200&type=restaurant&key=AIzaSyBq0ImMlJHsFIWZ0fKsoLQYOHhXwDbGiKU';
 
-class App extends Component {
+export default class App extends Component {
 
   constructor(props) {
     super(props);
@@ -18,7 +19,8 @@ class App extends Component {
         lon: ''
       },
       userInput: '',
-      menuData: []
+      menuData: [],
+      foodItems: []
     };
 
     this.fetchNearbyRestaurants = this.fetchNearbyRestaurants.bind(this);
@@ -36,7 +38,6 @@ class App extends Component {
   getUserLocation() {
     const self = this;
     function success(pos) {
-      // pass in function instead of object?
       self.setState({
         userCoords: {
           lat: pos.coords.latitude,
@@ -56,20 +57,21 @@ class App extends Component {
   // use Foursquare API to fetch all restaurants nearby after user location has been retrieved
   fetchNearbyRestaurants() {
     const self = this;
-    const {lat, lon} = this.state.userCoords;
+    const { lat, lon } = this.state.userCoords;
 
     // move to config?
     const fsRestUrl = `https://api.foursquare.com/v2/venues/search?categoryId=4d4b7105d754a06374d81259&ll=${lat},${lon}&client_id=${config.CLIENT_ID}&client_secret=${config.CLIENT_SECRET}&v=20170101`;
 
     axios.get(proxyUrl + fsRestUrl)
-      .then(function (res) {
+      .then(function(res) {
         self.fetchMenus(res);
       })
-      .catch(function (error) {
+      .catch(function(error) {
         console.log(error);
       });
   }
 
+  // of all restaurants fetched that have a menu, combine each restaurant's menu data and basic info to object and concat to state.menuData
   fetchMenus(res) {
     const self = this;
 
@@ -78,24 +80,26 @@ class App extends Component {
     }
 
     res.data.response.venues.forEach(venue => {
-      const {id, menu, name, location, contact, url} = venue;
+      const { id, menu, name, location, contact, url } = venue;
       if (menu) {
         axios.get(proxyUrl + fsMenuUrl(id))
-          .then(function (res) {
+          .then(function(res) {
             const menuData = res.data.response.menu.menus;
             self.setState((prevState, props) => ({
               menuData: [...prevState.menuData, {...menuData, id, name, location, contact, url}]
             }));
           })
-          .catch(function (error) {
+          .catch(function(error) {
             console.log(error);
           });
       }
     });
-
   }
 
+  // search each menu for user input
   searchMenus(userInput) {
+    const self = this;
+
     this.state.menuData.map(merchant => {
       if (merchant.count) {
         merchant.items.map(menu => {
@@ -104,10 +108,12 @@ class App extends Component {
               if (section.entries.count) {
                 section.entries.items.map(item => {
                   const itemName = item.name.toLowerCase();
-                  const price = item.price
-                  if (itemName.includes(this.state.userInput)) {
-                    console.log(merchant);
-                    console.log(itemName, price); 
+                  const { price, description } = item;
+                  if (price && itemName.includes(this.state.userInput)) {
+                    const { id, name, location, contact, url } = merchant;
+                    self.setState((prevState, props) => ({
+                      foodItems: [...prevState.foodItems, {itemName, price, description, id, name, location, contact, url}]
+                    }));
                   }
                 })
               }
@@ -125,23 +131,31 @@ class App extends Component {
 
   handleInputChange(event) {
     this.setState({ userInput: event.target.value });
-    console.log(this.state.userInput);
   }
 
   render() {
+
+    const foodItems = this.state.foodItems.map((foodItem, index) => {
+      console.log(foodItem);
+      return (
+        <FoodItem 
+          {...foodItem}
+          key={index}
+        />
+      );
+    });
+
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h2>Dish</h2>
         </div>
         <form>
           <input type="text" onChange={this.handleInputChange} value={this.state.userInput} />
           <button type="submit" onClick={this.handleInputSubmit}>Search</button>
         </form>
+        {foodItems}
       </div>
     );
   }
 }
-
-export default App;
