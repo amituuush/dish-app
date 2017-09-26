31660ae5f16feb987f26cfc98aaa1c3da8ad2915
Amit Ranan <ranan.amit@gmail.com>
2017-09-26 14:10:05 -0700

    cleaned up map component code

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,63 +1,44 @@
 import React from 'react';
-import { compose, withStateHandlers } from 'recompose';
+import { compose } from 'recompose';
 import { withScriptjs, withGoogleMap, GoogleMap, Marker, InfoWindow, OverlayView } from 'react-google-maps';
 import PropTypes from 'prop-types';
 
 import '../App.scss';
 
-const style = {
-
-}
-
-const getPixelPositionOffset = (width, height) => ({
-  x: -(width / 2),
-  y: -(height / 2),
-})
-
 const Map = compose(
-  withStateHandlers(() => ({
-    count: 0,
-  }), {
-    onClick: ({ count }) => () => ({
-      count: count + 1,
-    })
-  }),
   withScriptjs,
   withGoogleMap
 )(props =>
   <GoogleMap
     defaultZoom={props.defaultZoom}
     defaultCenter={props.center} >
-  {
-    props.markers.map((marker, index) => {
-      const { lat, lng } = marker.location;
-      const markerCenter = { lat, lng };
-      const { itemName, name, price, id, isOpen, url } = marker;
-      return (
-        <Marker
-          key={index}
-          position={markerCenter}
-          onClick={() => props.handleMarkerOpen(id)} >
-          { isOpen ?
-            <InfoWindow onCloseClick={() => props.handleMarkerClose(id)}>
-              <div className="info-window-gm">
-                <h3>{itemName}</h3>
-                <p className="info-window-price">${price}</p>
-                <a href={url} target="_blank"><p>{name}</p></a>
-              </div>
-            </InfoWindow>
-            : ''
-          }
-        </Marker>
-      );
-    })
-  }
-  <OverlayView
+    {
+      props.markers.map((marker, index) => {
+        const { lat, lng } = marker.location;
+        const markerCenter = { lat, lng };
+        const { itemName, name, price, id, isOpen, url } = marker;
+        return (
+          <Marker
+            key={index}
+            position={markerCenter}
+            onClick={() => props.handleMarkerOpen(id)} >
+            { isOpen ?
+              <InfoWindow onCloseClick={() => props.handleMarkerClose(id)}>
+                <div className="info-window-gm">
+                  <h3>{itemName}</h3>
+                  <p className="info-window-price">${price}</p>
+                  <a href={url} target="_blank"><p>{name}</p></a>
+                </div>
+              </InfoWindow>
+              : ''
+            }
+          </Marker>
+        );
+      })
+    }
+    <OverlayView
       position={{ lat: props.center.lat, lng: props.center.lng }}
-
       mapPaneName={OverlayView.OVERLAY_MOUSE_TARGET}
-
-      getPixelPositionOffset={getPixelPositionOffset}
     >
       <div style={{ width: '17px', height: '17px', borderRadius: '100%', background: `#4285F4`, border: `3px solid #fff`, boxShadow: '0px 0px 4px #282C34' }}></div>
     </OverlayView>
